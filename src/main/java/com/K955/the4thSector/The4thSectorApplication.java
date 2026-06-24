d1736808440d12c4fc739fd731df2641dfb27bab
krishsharma955 <krishsharma9005@gmail.com>
2026-06-24 16:18:59 +0530

    feat: Project Setup

---
+++ src/main/java/com/K955/the4thSector/The4thSectorApplication.java
@@ -0,0 +1,13 @@
+package com.K955.the4thSector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class The4thSectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(The4thSectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/K955/the4thSector/The4thSectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.K955.the4thSector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class The4thSectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
